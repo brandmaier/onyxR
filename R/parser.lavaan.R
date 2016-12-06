a8c8f25b8e3e3960aae9e0489979a48e5a8add87
Brandmaier <brandmaier@mpib-berlin.mpg.de>
2016-12-06 20:39:56 +0100

    added lavaan parser

---
+++ R/parser.lavaan.R
@@ -0,0 +1,60 @@
+parser.lavaan <- function(model) {
+
+lstr <- lavaanify(model)
+
+name <- "Name"
+
+xml <- paste( "<model name=\"",name,"\" specificationType=\"Onyx\" specificationVersion=\"1.0\">\n<graph>\n",sep="");
+
+known <- list()
+
+isknown <- function(key)
+{
+  return(key %in% names(known))
+}
+
+idx <- 1
+for (i in 1:dim(lstr)[1]) {
+  left <- lstr[i,2]
+  op <- lstr[i,3]
+  right <- lstr[i,4]
+  
+  latentleft <- TRUE
+  latentright <- TRUE;
+  if (op == "~=") { latentleft <- TRUE; latentright <- FALSE}
+#  if (op == "~~")
+  
+  if (!isknown(left)) {
+    lid <- known[[latentleft]]
+  } else {
+    xml <- paste(xml,"<Node caption=\"", left ,  "\" latent=\"",latentleft,"\" id=\"",
+                 (i),"\" />\n",sep="");
+    known[[latentleft]] <- idx
+    lid <- idx
+    idx <- idx + 1
+  }
+  
+  if (!isknown(right)) {
+      rid <- known[[latentright]]
+  } else {
+    xml <- paste(xml,"<Node caption=\"", right ,  "\" latent=\"",latentright,"\" id=\"",
+                 (i),"\" />\n",sep="");
+    known[[latentright]] <- idx
+    rid <- idx
+    idx <- idx + 1
+  }
+  
+  
+  
+  xml <- paste(xml,"<Edge sourceNodeId=\"",lid,"\"  targetNodeId=\"",
+    rid,"\" doubleHeaded=\"",doubleheaded,"\" fixed=\"",fixed,"\" ",pString," ",dString, " value=\"",value,"\" />\n", sep="")
+  
+  
+  return(xml);
+}
+  
+xml <- paste(xml, "\n</graph>\n</model>\n" ,sep="");
+  
+return(xml)
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # onyxR
-Starting Onyx from R 
+
+Calling Onyx GUI from R when using OpenMx or lavaan packages for SEM.
+
+## Install
+
+To install the gppmr package. Copy the following line into R:
+```{r, eval=FALSE}
+source('https://raw.githubusercontent.com/brandmaier/onyxR/master/tools/install.R')
+```
