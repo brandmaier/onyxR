d0ce32e49599eedc6be5888da20b76f8f8f10155
Brandmaier <brandmaier@mpib-berlin.mpg.de>
2016-12-07 10:07:36 +0100

    fixed issues with lavaan parser

--- R/onyx.R
+++ R/onyx.R
@@ -1,19 +1,32 @@
-onyx<-function(model=NULL, path="")
+onyx<-function(model=NULL, onyxfile=NULL)
 {
-  # the following part tries to find onyx.jar in the path of the script and the home directory.
-  if (nchar(path)==0) {
+  
+  # the following part tries to find onyx.jar from command line arguments.
+  # May be removed in future
+  if (is.null(onyxfile)) {
     options <- commandArgs(trailingOnly = F)
     filearg <- "--file="
-    scriptname <- sub(filearg, "", options[grep(filearg, options)])
-    path <- dirname(scriptname)
-    onyxpath <- paste(path,"onyx.jar",sep="")
-    if (!file.exists(onyxpath)) path <- "" 
+ 
+    if (length(grep(filearg, options))!=0) {
+      onyxfile <- sub(filearg, "", options[grep(filearg, options)])
+    }
+
+  }
+  
+  # if there is no Onyx jar, download it from official repository
+  if (is.null(onyxfile)) {
+    warning("Could not find a local Onyx version. Trying to download Onyx from the official repository.")
+    onyxfile <- tempfile()
+    download.file("http://onyx.brandmaier.de/onyx-1.0-937.jar",destfile = onyxfile)
+    if (!file.exists(onyxfile)) {
+      stop("Download failed!")
+    }
   }
   
   # catches if onyx.jar is not available
-  onyxpath <- paste(path,"onyx.jar",sep="")
-  if (!file.exists(onyxpath)) {
-    stop("file onyx.jar not found. You can download it at http:\\\\onyx.brandmaier.de.")
+  
+  if (!file.exists(onyxfile)) {
+    stop("Could not find Onyx executable (e.g., onyx.jar)! Please specify a path to the file or download Onyx from http:\\\\onyx.brandmaier.de.")
   }
   
   # stores onyx or lavaan parsed model in temp file and calls onyx with this file, or empty call. 
@@ -22,13 +35,13 @@ onyx<-function(model=NULL, path="")
 	if (inherits(model,"MxModel") || inherits(model,"MxRAMModel")) {
   		rep <- parser.OpenMx(model,"onyxR")
 	} else {
-		rep <- parser.lavaan(model,"onyxR")
+	  	rep <- parser.lavaan(model,"onyxR")
 	}
   	fn <- tempfile()
   	cat(rep, file=fn)
-  	cmd <- paste("java","-cp",onyxpath,"Master","--input-file ",fn)
+  	cmd <- paste("java","-cp",onyxfile,"Master","--input-file ",fn)
   } else {
-    cmd <- paste("java","-cp",onyxpath,"Master")
+    cmd <- paste("java","-cp",onyxfile,"Master")
   }
   
 	system(cmd,wait=F)
--- R/parser.lavaan.R
+++ R/parser.lavaan.R
@@ -2,7 +2,7 @@ parser.lavaan <- function(model, name="") {
 
 lstr <- lavaanify(model)
 
-xml <- paste( "<model name=\"",name,"\" specificationType=\"Onyx\" specificationVersion=\"1.0\">\n<graph>\n",sep="");
+xml <- paste( "<model name=\"",name,"\" specificationType=\"Onyx\" specificationVersion=\"1.0-500\">\n<graph>\n",sep="");
 
 known <- list()
 
@@ -11,7 +11,7 @@ isknown <- function(key)
   return(key %in% names(known))
 }
 
-idx <- 1
+idx <- 0
 for (i in 1:dim(lstr)[1]) {
   left <- lstr$lhs[i]
   op <- lstr$op[i]
@@ -20,14 +20,13 @@ for (i in 1:dim(lstr)[1]) {
   ustart <- lstr$ustart[i]
   latentleft <- TRUE
   latentright <- TRUE;
-  if (op == "~=") { latentleft <- TRUE; latentright <- FALSE}
-#  if (op == "~~")
+  if (op == "=~") { latentleft <- TRUE; latentright <- FALSE}
   
   if (isknown(left)) {
     lid <- known[[left]]
   } else {
-    xml <- paste(xml,"<Node caption=\"", left ,  "\" latent=\"",latentleft,"\" id=\"",
-                 (i),"\" />\n",sep="");
+    xml <- paste(xml,"<node caption=\"", left ,  "\" latent=\"",latentleft,"\" id=\"",
+                 (idx),"\" />\n",sep="");
     known[[left]] <- idx
     lid <- idx
     idx <- idx + 1
@@ -37,30 +36,44 @@ for (i in 1:dim(lstr)[1]) {
       rid <- known[[right]]
   } else {
     xml <- paste(xml,"<Node caption=\"", right ,  "\" latent=\"",latentright,"\" id=\"",
-                 (i),"\" />\n",sep="");
+                 (idx),"\" />\n",sep="");
     known[[right]] <- idx
     rid <- idx
     idx <- idx + 1
   }
   
   if (op == "~~")
-    doubleheaded <- TRUE 
+    doubleheaded <- "true" 
   else
-    doubleheaded <- FALSE
+    doubleheaded <- "false"
   
-  if (free == 0)
-    fixed <- TRUE
-  else 
-    fixed <- FALSE
+  if (free == 0) {
+    fixed <- "true"
+    pString <- ""
+    }
+  else {
+    fixed <- "false"
+    pname <- lstr$plabel[i]
+    pString <- paste("parameterName=\",pname,\"",sep="")
+  }
   
-  pString <- ""
-  dString <- "" # no def. vars.
   
-  value <- 0
+  
+  aString <- "arrowHead=\"1\" "#definitionVariable=\"false\""
+  
+  value <- 1
   if (!is.na(ustart)) value <- ustart
   
-  xml <- paste(xml,"<Edge sourceNodeId=\"",lid,"\"  targetNodeId=\"",
-    rid,"\" doubleHeaded=\"",doubleheaded,"\" fixed=\"",fixed,"\" ",pString," ",dString, " value=\"",value,"\" />\n", sep="")
+  # some postprocessing. Onyx does not like fixed path with zero values
+  if ((op == "~~")) {
+    if (value==0 && free==0) {
+      value <- 1
+    }
+  }
+  
+  xml <- paste(xml,"<edge sourceNodeId=\"",lid,"\"  targetNodeId=\"",
+    rid,"\" doubleHeaded=\"",doubleheaded,"\" fixed=\"",fixed,"\" ",
+    pString," ",aString, " value=\"",value,"\" />\n", sep="")
 } 
   
  
